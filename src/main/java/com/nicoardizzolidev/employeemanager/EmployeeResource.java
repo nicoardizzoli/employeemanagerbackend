6fb83084cab95c27fd5cd3fb9120b494f46727dd
nicoardizzoli <nicoardizzoli.dev@gmail.com>
2021-11-28 11:44:52 -0300

    solved problem with delete method.

--- src/main/java/com/nicoardizzolidev/employeemanager/EmployeeResource.java
+++ src/main/java/com/nicoardizzolidev/employeemanager/EmployeeResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -51,6 +52,7 @@ public class EmployeeResource {
 		return new ResponseEntity<Employee>(updateEmployee, HttpStatus.CREATED);
 	}
 	
+	@Transactional
 	@DeleteMapping("/delete/{id}")
 	public ResponseEntity<?> updateEmployee(@PathVariable Long id){
 		employeeService.deleteEmployee(id);
